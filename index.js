456a23c1ca9816b7396a2fd57c5af033446fc17e
Rune Halvorsen <runefh@gmail.com>
2017-02-26 11:00:05 +0100

    Moar

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const { crawl } = require('./lib/crawler');
 const { resolve } = require('path');
 
-
 // console.log(JSON.stringify(crawl(resolve('./example'))))
-
 
 const { MockServer } = require('./lib/middleware');
 
-
-const tree = crawl(resolve('./example'));
+const tree = crawl(resolve('./ex2'));
 const server = new MockServer(tree);
-
 
 const express = require('express');
 const app = express();
@@ -20,19 +16,15 @@ app.get('/', function(req, res) {
 });
 
 app.use(server.router());
-console.log(server.routes)
-
+console.log(server.routes);
 
 app.listen(3000, function() {
     console.log('Example app listening on http://localhost:3000');
 });
 
-
 // var prettyjson = require('prettyjson');
 // console.log(JSON.stringify(parseFileSystem(resolve('./example'))))
-
 // const router = createRouter('./example');
-
 // router('/example', 1111);
 // router('/example/searches', 1112);
 // router('/example/what', 1113);
