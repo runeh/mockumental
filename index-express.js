99ce582a2b894a4f66b0bf3644e1a0ab2ace0235
Rune Halvorsen <runefh@gmail.com>
2017-02-27 06:28:21 +0100

    moar

--- index-express.js
+++ index-express.js
@@ -8,7 +8,7 @@ app.get('/', function(req, res) {
     res.send('Hello World!');
 });
 
-const mock = middleware('./ex2');
+const mock = middleware('./example');
 
 mock.paths.forEach(({ path, method }) => {
     console.log(method, path);
