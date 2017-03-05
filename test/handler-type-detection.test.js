18be64aa28e2af3f746e71d5a6d852184744239d
Rune Finstad Halvorsen <runefh@gmail.com>
2017-03-05 23:57:24 +0100

    Parse dynaic type

---
+++ test/handler-type-detection.test.js
@@ -0,0 +1,22 @@
+const { getHandlerType } = require('../lib/dirparser/handlerparser');
+
+test('static json type', () => {
+    expect(getHandlerType('ok.json'))
+        .toBe('static');
+});
+
+test('static html type', () => {
+    expect(getHandlerType('ok.html'))
+        .toBe('static');
+});
+
+test('static html type, works in concert with other opts', () => {
+    expect(getHandlerType('ok.POST.PUT.204.html'))
+        .toBe('static');
+});
+
+test('dynamic json type', () => {
+    expect(getHandlerType('ok.json.js'))
+        .toBe('dynamic');
+});
+
