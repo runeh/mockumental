d317f166f7c11922f8f9f03dda67c1761d12a81b
Rune Finstad Halvorsen <runefh@gmail.com>
2017-03-07 13:29:57 +0100

    Nocli flag

--- bin/mockumental-serve.js
+++ bin/mockumental-serve.js
@@ -13,9 +13,10 @@ program
     .usage('[options] <directory>')
     .option('-u, --interactive', 'show interactive cli ui')
     .option('-p, --port <port>', 'port to bind to (default 3031)', parseInt, 3031)
-    .option('-i, --ip <ip>', 'host / ip to bind to (default localhost', 'localhost')
+    .option('-a, --address <adress>', 'address to bind to (default localhost', 'localhost')
     .option('-m, --mount <path>', 'mount point', '/')
     .option('-c, --cors', 'enable CORS')
+    .option('-i, --nocli','don\'t show interactive CLI', false)
     .parse(process.argv);
 
 const mockRootDir = resolve(program.args.shift() || './');
@@ -48,9 +49,11 @@ if (program.cors) {
 
 app.use(mountPoint, mocker.router);
 
-const server = app.listen(program.port, program.ip, () => {
+const server = app.listen(program.port, program.address, () => {
     const { address, port } = server.address();
     console.log(`Hosting ${ mockRootDir } on http://${ address }:${ port }${ mountPoint }`);
     console.log(`Admin UI on http://${ address }:${ port }${ mountPoint }__admin`);
-    aquireGuiSelection();
+    if (!program.nocli) {
+        aquireGuiSelection();
+    }
 });
