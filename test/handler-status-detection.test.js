ab3b6b4633f1f5076d05b78868bec4ea7ce5ed63
Rune Finstad Halvorsen <runefh@gmail.com>
2017-03-08 15:25:03 +0100

    Removed some fixmes

--- test/handler-status-detection.test.js
+++ test/handler-status-detection.test.js
@@ -10,7 +10,6 @@ test('explicit dotted status 200', () => {
         .toBe(505);
 });
 
-// fixme: figure out semantics of this
 test('status as file name', () => {
     expect(inferStatusCode('506.json'))
         .toBe(506);
