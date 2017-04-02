9527504640f9d4a9f573db873a26e5f297b66907
Rune Finstad Halvorsen <runefh@gmail.com>
2017-04-02 11:33:45 +0200

    Added some express integration tests

---
+++ test/express-integration.test.js
@@ -0,0 +1,57 @@
+const { resolve } = require('path');
+
+const express = require('express');
+const request = require('supertest');
+
+const { ExpressMockumental } = require('../lib/cli-utils/express');
+
+const mockRootDir = resolve(__dirname, 'trees', 'express');
+
+function setup() {
+    const app = express();
+    const mocker = new ExpressMockumental(mockRootDir);
+    app.use('/mocks', mocker.router);
+    return [app, mocker];
+} 
+
+test('smoke test 1', () => {
+    const [app, mocker] = setup();
+    return request(app)
+        .get('/mocks/')
+        .expect('Content-Type', /html/)
+        .expect(200)
+        .expect(/All good get/)
+});
+
+test('smoke test 2', () => {
+    const [app, mocker] = setup();
+    mocker.activateHandler('2');
+    return request(app)
+        .get('/mocks/')
+        .expect('Content-Type', /html/)
+        .expect(500)
+        .expect(/internal server error/)
+});
+
+test('smoke test 3', () => {
+    const [app, mocker] = setup();
+    return request(app)
+        .post('/mocks/')
+        .expect('Content-Type', /html/)
+        .expect(200)
+        .expect(/All good post data/)
+});
+
+test('smoke test 4', () => {
+    const [app, mocker] = setup();
+    mocker.activateHandler('3');
+    return request(app)
+        .get('/mocks/')
+        .expect('Content-Type', /text/)
+        .expect(200)
+        .expect(/hello world/)
+});
+
+// .then(response => {
+//           assert(response.body.email, 'foo@bar.com')
+//       })
---
+++ test/trees/express/3_dynamic.hid-3.txt.js
@@ -0,0 +1,3 @@
+module.exports = function(params, req, memo) {
+    return 'hello world';
+}
